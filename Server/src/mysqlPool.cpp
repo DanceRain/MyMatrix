ab5690ead7e660807b6975b840eb24d187aa7445
DanceRain <1477364283@qq.com>
2022-02-18 22:41:12 +0800

    database connetion pool

---
+++ Server/src/mysqlPool.cpp
@@ -0,0 +1,152 @@
+#include "mysqlPool.h"
+#include <cppconn/connection.h>
+#include <cppconn/exception.h>
+#include <exception>
+#include <memory>
+#include <pthread.h>
+#include <iostream>
+
+const string g_sdefaultHost = "localhost";
+const string g_sdefaultUser = "neo";
+const string g_sdefaultPass = "123456";
+const int g_imaxSize = 20;
+
+
+ConnectionPool::ConnectionPool(const std::string& host, 
+        const std::string& userName, 
+        const std::string& pwd, int maxSize):
+    m_host(host), m_userName(userName), m_pwd(pwd), _MAXSIZE_(maxSize)
+{
+    try
+    {
+        m_pDriver = get_driver_instance();
+    }
+    catch
+    {
+        
+    }
+    this->InitConnectionPool(maxSize / 2);
+}
+
+ConnectionPool& ConnectionPool::getConnectionPool()
+{
+    if(onlyInstance.get() == 0)
+    {
+        onlyInstance.reset(new ConnectionPool(g_sdefaultHost, g_sdefaultUser,
+                    g_sdefaultPass, g_imaxSize));
+    }
+    return *(onlyInstance.get());
+}
+
+void ConnectionPool::InitConnectionPool(unsigned ConnectionSize)
+{
+    Connection* pConn;
+    pthread_mutex_lock(&lock);
+    for(int i = 0; i < ConnectionSize; ++i)
+    {
+        pConn = this->CreateOneConnection();
+        if(pConn)
+        {
+            m_connList.push_back(pConn);
+            ++(_CURRENTSIZE_);
+        }
+        else
+        {
+            cerr << "can't create database connection" << endl;
+        }
+    }
+    pthread_mutex_unlock(&lock);
+}
+
+Connection* ConnectionPool::CreateOneConnection()
+{
+    Connection* pConn;
+    try{
+        pConn = m_pDriver->connect(m_host, m_userName, m_pwd);
+        return pConn;
+    }catch(SQLException& e)
+    {
+        cerr << e.what() << endl;
+        return nullptr;
+    }
+}
+
+
+Connection* ConnectionPool::getConnectionFromPool()
+{
+    Connection* pConn;
+    pthread_mutex_lock(&lock);
+    if(m_connList.size() > 0)
+    {
+        pConn = m_connList.front();
+        m_connList.pop_front();
+        if(pConn->isClosed())
+        {
+            delete pConn;
+            pConn = this->CreateOneConnection();
+        }
+        if(nullptr == pConn)
+        {
+            //新连接创建错误，当前连接池剩余连接 -1
+            --_CURRENTSIZE_;
+        }
+        pthread_mutex_unlock(&lock);
+        return pConn;
+    }
+    else
+    {
+        if(_CURRENTSIZE_ < _MAXSIZE_)
+        {
+            pConn = this->CreateOneConnection();
+            if(pConn)
+            {
+                ++_CURRENTSIZE_;
+                pthread_mutex_unlock(&lock);
+                return pConn;
+            }
+            else
+            {
+                pthread_mutex_unlock(&lock);
+                return nullptr;
+            }
+        }
+        else
+        {
+            pthread_mutex_unlock(&lock);
+            return nullptr;
+        }
+    }
+}
+
+void ConnectionPool::TerminateConnectionPool()
+{
+    list<Connection*>::iterator it;
+    pthread_mutex_lock(&lock);
+    for(it = m_connList.begin(); it != m_connList.end(); ++it)
+    {
+        this->TerminateConnectionFromPool(*it);
+    }
+    _CURRENTSIZE_ = 0;
+    m_connList.clear();
+    pthread_mutex_unlock(&lock);
+}
+
+void ConnectionPool::TerminateConnectionFromPool(Connection* pConn)
+{
+    if(pConn)
+    {
+        try{
+            pConn->close();
+        }catch(SQLException& e)
+        {
+            cerr << e.what() << endl;
+        }
+    }
+    delete pConn;
+}
+
+unsigned ConnectionPool::getCurrentSize()
+{
+    return _CURRENTSIZE_;
+}
+
---
+++ Server/src/mysqlPool.h
@@ -0,0 +1,59 @@
+#ifndef _MYSQLPOOL_H_
+#define _MYSQLPOOL_H_
+
+#include <memory>
+#include <string>
+#include <list>
+#include <pthread.h>
+#include <cppconn/driver.h>
+#include <cppconn/connection.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+using namespace sql;
+
+class ConnectionPool
+{
+public:
+    ConnectionPool(ConnectionPool&&) = delete;
+    ConnectionPool(const ConnectionPool&) = delete;
+    ConnectionPool& operator=(const ConnectionPool&) = delete;
+    ConnectionPool& operator=(const ConnectionPool&&) = delete;
+
+private:
+    ConnectionPool(const string& host, const string& userName, const string& pwd, int maxSize);
+    ~ConnectionPool() = default;
+    void InitConncetionPool(unsigned connectionSize);
+    void TerminateConnectionPool();
+    void TerminateConnectionFromPool(Connection* p);
+    Connection* CreateOneConnection();
+    
+public:
+    static ConnectionPool& getConnectionPool();
+    Connection* getConnectionFromPool();
+    void releaseConnection(Connection* pConn);
+    unsigned getCurrentSize();
+    
+private:
+    const unsigned _MAXSIZE_;
+    unsigned _CURRENTSIZE_ = 0;
+
+private:
+    string m_userName;
+    string m_pwd;
+    string m_host;
+    list<Connection*> m_connList;
+    Driver *m_pDriver;
+private:
+    friend class unique_ptr<ConnectionPool>;
+    static unique_ptr<ConnectionPool> onlyInstance;
+private:
+    pthread_mutex_t lock;
+    
+};
+
+
+#endif
