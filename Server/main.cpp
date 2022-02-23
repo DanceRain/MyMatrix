d7d6556cfb76746da6faac2b5b9fb4456a51a187
DanceRain <1477364283@qq.com>
2022-02-23 22:48:58 +0800

    user websocket in server

--- Server/main.cpp
+++ Server/main.cpp
@@ -4,13 +4,28 @@
 #include <iostream>
 #include <memory>
 #include "src/mysqlPool.h"
+#include <websocketpp/common/connection_hdl.hpp>
+#include <websocketpp/connection_base.hpp>
+#include <websocketpp/logger/levels.hpp>
+#include <websocketpp/roles/server_endpoint.hpp>
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
 
 using namespace std;
 using namespace sql;
 
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+void on_message(websocketpp::connection_hdl, server::message_ptr msg)
+{
+    cout << msg->get_payload() << endl;
+}
+
+
 int main()
 {
-    Connection* pConn = ConnectionPool::getConnectionPool().getConnectionFromPool();
+/*    Connection* pConn = ConnectionPool::getConnectionPool().getConnectionFromPool();
     if(pConn != nullptr)
     {
         cout << "Succesful! Current size is " << ConnectionPool::getConnectionPool().getCurrentSize() << endl;
@@ -24,6 +39,16 @@ int main()
             cout << "password: " << pRes->getString("user_pwd");
         }
 
-    }
+    }*/
+    server print_server;
+    print_server.set_message_handler(&on_message);
+    print_server.set_access_channels(websocketpp::log::alevel::all);
+    print_server.set_error_channels(websocketpp::log::elevel::all);
+    
+    print_server.init_asio();
+    print_server.listen(9999);
+    print_server.start_accept();
+    cout <<"hello" << endl;
+    print_server.run();
     return 0;
 }
